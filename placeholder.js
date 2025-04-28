4738710f924d454139f99f265fd7a29b2de29074
Gim Liu <gim.liu@crick.ac.uk>
2025-04-28 13:23:23 +0100

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -1,1 +1,21 @@
-console.log("CodeQL enabled");
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+});
+
+app.get('/search', function (req, res) {
+  const term = req.query.term;
+  // 🚨 Vulnerable: SQL injection via unsanitized input
+  connection.query("SELECT * FROM products WHERE name = '" + term + "'", function (err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.listen(3000);

2a45023459a568a41e7623b9be52b76873b5495b
Gim Liu <gim.liu@crick.ac.uk>
2025-11-11 13:47:56 +0000

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,9 @@
+GitHub PAT: "\bghp_[0-9a-fA-F]{8,40}\b"
+
+AWS Access Key ID: "\bAKIA[0-9A-Z]{16}\b"
+
+Slack bot token: "\bxoxb-[0-9]{12,}-[0-9A-Za-z]+"
+
+Google API key: "\bAIza[0-9A-Za-z-_]{35}\b"
+
+PEM private key presence: -----BEGIN (RSA )?PRIVATE KEY-----
